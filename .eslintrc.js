3ce9792ad9212b94966433fed7011d8b0caee824
Luis Corales <lcorales@stackbuilders.com>
2022-08-30 14:59:51 -0500

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+  },
+};
